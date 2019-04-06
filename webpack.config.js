d8fd6cb84666537b670520086963ba684234d592
edelprior <edelprior@hotmail.com>
2019-04-06 13:23:47 +0100

    server

--- webpack.config.js
+++ webpack.config.js
@@ -43,8 +43,8 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-           query: { presets: ['@babel/preset-env', '@babel/preset-react']
-         }
+          query: { presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       },
       {
